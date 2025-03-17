717e903843fa5b9368ca95898c34111c78357161
Kaiyuan Han <hermit_han@163.com>
2025-03-17 20:11:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# taf-bwa
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/lh3/bwa
+
+Burrow-Wheeler Aligner for short-read alignment (see minimap2 for long-read alignment).
---
+++ bwa.info.lisp
@@ -0,0 +1,13 @@
+(:name           "bwa")
+(:repo           "taffish-org/bwa")
+(:author         "KaiyuanHan")
+(:version        "0.7.18")
+(:intro          "Burrow-Wheeler Aligner for short-read alignment (see minimap2 for long-read alignment)")
+(:dependencies)
+(:functions      "bwa"
+                 "qualfa2fq.pl"
+                 "xa2multi.pl"
+                 "run-HLA"
+                 "run-bwamem"
+                 "run-gen-ref"
+                 "typeHLA.sh")
